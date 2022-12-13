0453f51e8192e00c586b4575dea455fe63eb1dd9
Alfhhsn <aisyahusnalifah30@gmail.com>
2022-12-13 15:24:27 +0700

    ais update

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="sign-in.css">
+    <link rel="stylesheet" href="css/sign-in.css">
   </head>
   <body>
 
@@ -40,7 +40,7 @@
                 <button type="button" class="btn btn-primary sign-up">Sign Up</button>
             </div>
             <div class="col-6">
-                
+                <img src="img/sign up image.png" alt="">
             </div>
         </div>
     </div>
