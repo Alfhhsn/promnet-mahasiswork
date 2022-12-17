12cad12aa08107c87cd3c22783b2f204c947fb5a
b18nabil <b18nabil@gmail.com>
2022-12-17 14:36:36 +0700

    remember login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mahasiswork | Login</title>
+    <title>MahasisWork | Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="css/login.css">
   </head>
@@ -31,7 +31,7 @@
           </section>
           <section class="remember">
             <input class="form-check-input mt-0 klik" type="checkbox" value="" aria-label="Checkbox for following text input">
-            <span class="remember-text">Remenber Login</span>
+            <span class="remember-text">Remember Login</span>
           </section>
           <button type="button" class="btn btn-primary login-btn">Login</button>
           
